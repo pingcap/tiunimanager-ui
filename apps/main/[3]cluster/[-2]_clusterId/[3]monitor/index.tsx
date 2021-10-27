1f8b487d8bf45a899d45fd3a39ae7ba6a80f5b08
unbyte <i@shangyes.net>
2021-10-27 13:54:02 +0800

    fetch services url from api

--- apps/main/[3]cluster/[-2]_clusterId/[3]monitor/index.tsx
+++ apps/main/[3]cluster/[-2]_clusterId/[3]monitor/index.tsx
@@ -1,7 +1,12 @@
 import { ExternalService } from '@/components/ExternalService'
+import { useQueryClusterExternalService } from '@/api/hooks/cluster'
+import { useClusterContext } from '@apps/main/[3]cluster/[-2]_clusterId/context'
 
 export default function () {
-  return (
-    <ExternalService src={import.meta.env.VITE_TIDB_MONITOR_URL} mode="card" />
-  )
+  const { clusterId, clusterName } = useClusterContext()
+  const { data } = useQueryClusterExternalService({ id: clusterId! })
+  const grafanaUrl = data?.data.data?.grafanaUrl
+    ? `${data?.data.data?.grafanaUrl}/d/000000012/${clusterName}-tidb-summary?orgId=1&kiosk=tv`
+    : ''
+  return <ExternalService src={grafanaUrl} mode="card" />
 }
