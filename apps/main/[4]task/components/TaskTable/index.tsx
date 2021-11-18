808646b59a461a87bc3972d0d4033a7f376c3057
Yuiham <yuiham.chan@gmail.com>
2021-11-18 18:28:54 +0800

    feat(task): support the "cancelled" status display

--- apps/main/[4]task/components/TaskTable/index.tsx
+++ apps/main/[4]task/components/TaskTable/index.tsx
@@ -113,6 +113,7 @@ function getColumns(t: TFunction<'model'>): ProColumns<TaskWorkflowInfo>[] {
         '1': { text: t('model:task.status.processing'), status: 'Processing' },
         '2': { text: t('model:task.status.finished'), status: 'Success' },
         '3': { text: t('model:task.status.error'), status: 'Error' },
+        '4': { text: t('model:task.status.cancelled'), status: 'Default' },
       },
     },
     {
