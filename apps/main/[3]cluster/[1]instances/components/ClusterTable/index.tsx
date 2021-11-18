b95dcb9f4b49b1fbb454a49139eec0124e0c94f7
Yuiham Chan <Yuiham@users.noreply.github.com>
2021-11-18 13:49:39 +0800

    fix(cluster): correct the boot onConfirm callback (#32)

--- apps/main/[3]cluster/[1]instances/components/ClusterTable/index.tsx
+++ apps/main/[3]cluster/[1]instances/components/ClusterTable/index.tsx
@@ -456,7 +456,7 @@ function getColumns(
     },
     {
       title: t('columns.actions'),
-      width: 200,
+      width: 120,
       key: 'actions',
       valueType: 'option',
       render(_, record) {
@@ -476,7 +476,7 @@ function getColumns(
               onConfirm={async () => {
                 const hide = message.loading(t('boot.loading'), 0)
 
-                return await bootAction('boot', record.clusterId!)
+                await bootAction('boot', record.clusterId!)
 
                 hide()
               }}
