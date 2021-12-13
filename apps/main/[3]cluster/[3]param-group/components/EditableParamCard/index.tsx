4789cdd7e3c883831b7c94b9a4501441249b63d8
Yuiham <yuiham.chan@gmail.com>
2021-12-13 23:38:01 +0800

    feat(cluster/param-group): add editable parameter card component

---
+++ apps/main/[3]cluster/[3]param-group/components/EditableParamCard/index.tsx
@@ -0,0 +1,301 @@
+import { FC, useState, useMemo, useEffect, useRef } from 'react'
+import { TFunction } from 'react-i18next'
+import { loadI18n, useI18n } from '@i18n-macro'
+import { Card, Form, FormInstance } from 'antd'
+import { ActionType, ProColumns } from '@ant-design/pro-table'
+import HeavyTable from '@/components/HeavyTable'
+import { ParamItemDetail } from '@/api/model'
+import styles from './index.module.less'
+import { EditOutlined } from '@ant-design/icons'
+
+loadI18n()
+
+function getColumns(
+  t: TFunction<''>,
+  form: FormInstance,
+  saveKey: (key: number) => void
+) {
+  const columns: ProColumns<ParamItemDetail>[] = [
+    {
+      title: t('model:clusterParam.property.name'),
+      width: 220,
+      dataIndex: 'name',
+      key: 'name',
+      fixed: 'left',
+      editable: false,
+    },
+    {
+      title: t('model:clusterParam.property.reboot'),
+      width: 80,
+      dataIndex: 'hasReboot',
+      key: 'reboot',
+      render(_, record) {
+        return record.hasReboot!
+          ? t('model:clusterParam.reboot.true')
+          : t('model:clusterParam.reboot.false')
+      },
+      editable: false,
+    },
+    {
+      title: t('model:clusterParam.property.range'),
+      width: 200,
+      key: 'range',
+      ellipsis: true,
+      renderText(_, record) {
+        const hashmap: Record<number, (r: string[], u?: string) => string> = {
+          0: (range: string[], unit = '') =>
+            `${range[0]}${unit} ~ ${range[1]}${unit}`,
+          1: (range: string[]) => range.join(', '),
+          2: (range: string[]) => `${range[0]}, ${range[1]}`,
+          3: (range: string[], unit = '') =>
+            `${range[0]}${unit} ~ ${range[1]}${unit}`,
+          4: (range: string[]) => range.join(', '),
+        }
+
+        return Array.isArray(record.range)
+          ? hashmap[record.type!]?.(record.range, record.unit)
+          : null
+      },
+      editable: false,
+    },
+    {
+      title: t('model:clusterParam.property.default'),
+      width: 160,
+      dataIndex: 'defaultValue',
+      key: 'defaultValue',
+      render(node, record, idx, action) {
+        return (
+          <span
+            onClick={() => {
+              form.resetFields([record.paramId!])
+              action?.startEditable(record.paramId!)
+              saveKey(record.paramId!)
+            }}
+            style={{ cursor: 'pointer' }}
+          >
+            <EditOutlined /> {record.defaultValue}
+          </span>
+        )
+      },
+      formItemProps: () => {
+        // const recordValType = config.entity.type
+
+        return {
+          rules: [{ required: true, message: t('card.rules.required') }],
+        }
+      },
+    },
+    {
+      title: t('columns.actions'),
+      valueType: 'option',
+      width: 100,
+      render: (node, record, idx, action) => {
+        return (
+          <a
+            key="editable"
+            onClick={() => {
+              form.resetFields([record.paramId!])
+              action?.startEditable?.(record.paramId!)
+              saveKey(record.paramId!)
+            }}
+          >
+            {t('actions.edit')}
+          </a>
+        )
+      },
+    },
+    {
+      title: t('model:clusterParam.property.desc'),
+      dataIndex: 'description',
+      key: 'description',
+      ellipsis: true,
+      editable: false,
+    },
+  ]
+
+  return columns
+}
+
+const getRowKey = (record: ParamItemDetail) => record.paramId!
+
+const TableOptions = {
+  density: false,
+  fullScreen: false,
+  setting: false,
+  reload: false,
+}
+
+interface ParamCardProps {
+  loading: boolean
+  data?: ParamItemDetail[]
+  onEdit: (editing: boolean) => void
+  onSave: (paramList: ParamItemDetail[]) => void
+}
+
+type ComponentParamMap = {
+  [k: string]: ParamItemDetail[]
+}
+
+const ParamCard: FC<ParamCardProps> = ({ loading, data, onEdit, onSave }) => {
+  const componentParamMap = useMemo(() => {
+    if (!Array.isArray(data)) {
+      return {}
+    }
+
+    const hashmap = data.reduce((prev, item) => {
+      const { componentType = '' } = item
+
+      if (!componentType) {
+        return prev
+      }
+
+      if (!Array.isArray(prev[componentType])) {
+        prev[componentType] = [item]
+      } else {
+        prev[componentType].push(item)
+      }
+
+      return prev
+    }, {} as ComponentParamMap)
+
+    console.log('datasource changed')
+
+    return hashmap
+  }, [data])
+
+  const tabList = useMemo(
+    () =>
+      Object.keys(componentParamMap).map((name) => ({
+        key: name,
+        tab: name,
+      })),
+    [componentParamMap]
+  )
+
+  const [activeTab, setActiveTab] = useState(tabList?.[0]?.key)
+
+  const [editingKey, setEditingKey] = useState<number>()
+
+  const [tableDataMap, setTableDataMap] = useState<{
+    [k: string]: ParamItemDetail[]
+  }>({})
+
+  const { t, i18n } = useI18n()
+
+  const [paramForm] = Form.useForm()
+
+  const columns = useMemo(
+    () => getColumns(t, paramForm, setEditingKey),
+    [i18n.language, paramForm]
+  )
+
+  const editorRef = useRef<ActionType>()
+
+  useEffect(() => {
+    if (loading) {
+      return
+    }
+
+    const dataMap = Object.entries(componentParamMap).reduce(
+      (prev, [name, data]) => {
+        if (!name) {
+          return prev
+        }
+
+        prev[name] = data.concat()
+
+        return prev
+      },
+      {} as { [k: string]: ParamItemDetail[] }
+    )
+
+    setTableDataMap(dataMap)
+    setActiveTab(Object.keys(componentParamMap)[0])
+  }, [loading, componentParamMap])
+
+  useEffect(() => {
+    if (typeof editingKey === 'number') {
+      editorRef.current?.cancelEditable(editingKey)
+      setEditingKey(undefined)
+    }
+  }, [data])
+
+  useEffect(() => {
+    const isEditing = (editingKey ?? undefined) !== undefined
+
+    onEdit(isEditing)
+  }, [editingKey])
+
+  useEffect(() => {
+    const changedParamList = Object.values(tableDataMap).reduce(
+      (prev, list) => prev.concat(list),
+      []
+    )
+
+    onSave(changedParamList)
+  }, [tableDataMap])
+
+  return (
+    <Card
+      className={styles.paramCard}
+      title={t('card.title')}
+      size="small"
+      tabList={tabList}
+      activeTabKey={activeTab}
+      tabProps={{ size: 'middle' }}
+      onTabChange={(key) => {
+        if (typeof editingKey === 'number') {
+          editorRef.current?.cancelEditable(editingKey)
+          setEditingKey(undefined)
+        }
+
+        setActiveTab(key)
+      }}
+    >
+      <HeavyTable
+        loading={loading}
+        actionRef={editorRef}
+        dataSource={tableDataMap[activeTab]}
+        tooltip={false}
+        columns={columns}
+        pagination={false}
+        options={TableOptions}
+        rowKey={getRowKey}
+        scroll={{ x: 1200, y: 600 }}
+        editable={{
+          type: 'single',
+          form: paramForm,
+          actionRender: (row, config, dom) => [dom.save, dom.cancel],
+          onSave: async (paramId, editedRow) => {
+            setTableDataMap((prev) => {
+              const mapKey = editedRow.componentType!
+              const prevTableData = prev[mapKey]
+              const newTableData = prevTableData.map((row) => {
+                if (row.paramId !== paramId) {
+                  return row
+                }
+
+                return {
+                  ...row,
+                  defaultValue: editedRow.defaultValue,
+                }
+              })
+
+              return {
+                ...prev,
+                [mapKey]: newTableData,
+              }
+            })
+
+            setEditingKey(undefined)
+          },
+          onCancel: async () => {
+            setEditingKey(undefined)
+          },
+        }}
+      />
+    </Card>
+  )
+}
+
+export default ParamCard
