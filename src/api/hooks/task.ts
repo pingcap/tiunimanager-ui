ef74bbb9d08de5123b248c288b8b5c58e622ef99
Yuiham <yuiham.chan@gmail.com>
2021-12-20 19:20:31 +0800

    refactor(api/hooks): update the "useQueryTasks" hook

--- src/api/hooks/task.ts
+++ src/api/hooks/task.ts
@@ -1,22 +1,24 @@
 import { APIS, PartialUseQueryOptions } from '@/api/client'
 import { useQuery } from 'react-query'
+import { TaskWorkflowStatus } from '../model'
 
 export const CACHE_TASK = 'tasks'
 
 export function useQueryTasks(
   query: {
-    clusterId?: string
+    bizId?: string
     page?: number
     pageSize?: number
-    status?: number
+    status?: TaskWorkflowStatus
     keyword?: string
   },
   options?: PartialUseQueryOptions
 ) {
-  const { clusterId, page, pageSize, status, keyword } = query
+  const { bizId, page, pageSize, status, keyword } = query
+
   return useQuery(
-    [CACHE_TASK, clusterId, page, pageSize, status, keyword],
-    () => APIS.Task.flowworksGet(clusterId, keyword, page, pageSize, status),
+    [CACHE_TASK, bizId, page, pageSize, status, keyword],
+    () => APIS.Task.workflowGet(bizId, keyword, page, pageSize, status),
     options
   )
 }
