c8c94f59e95618a38aeea1bc9464c91f38ffe0ee
Yuiham <yuiham.chan@gmail.com>
2021-12-30 18:16:55 +0800

    feat(api): use cluster data replication APIs

--- src/api/client.ts
+++ src/api/client.ts
@@ -13,6 +13,7 @@ import {
   ClusterImportApi,
   ClusterLogApi,
   ClusterParametersApi,
+  ChangeFeedApi as ClusterDataReplicationApi,
   Configuration,
   KnowledgeApi,
   PlatformApi,
@@ -68,6 +69,11 @@ function initApis(basePath: string, axiosInstance: AxiosInstance) {
       axiosInstance
     ),
     ClusterBackups: new ClusterBackupApi(
+      configuration,
+      undefined,
+      axiosInstance
+    ),
+    ClusterDataReplication: new ClusterDataReplicationApi(
       configuration,
       undefined,
       axiosInstance
