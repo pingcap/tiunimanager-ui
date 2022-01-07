597d52e54bf5359d9adc1bcc0a47001f74f72e3b
unbyte <i@shangyes.net>
2022-01-07 15:41:22 +0800

    refine(auth): better feedback and message (#90)

--- src/api/interceptors/index.ts
+++ src/api/interceptors/index.ts
@@ -1,18 +1,26 @@
 import { AxiosError, AxiosResponse } from 'axios'
 import { useErrorNotification } from '@/components/ErrorNotification'
 import { useSuccessNotification } from '@/components/SuccessNotification'
+import { getAuthState } from '@store/auth'
 
 // for all 4xx, 5xx responses
 export const onErrorResponse = (error: AxiosError) => {
-  if (!error.config.skipInterceptors) {
+  if (!error.config.skipNotifications && !error.config.skipErrorNotification) {
     useErrorNotification(error)
+  }
+  if (error.response?.status === 401) {
+    getAuthState().logout()
   }
   return Promise.reject(error)
 }
 
 // for responses to non-GET requests
 export const onSuccessResponse = (resp: AxiosResponse) => {
-  if (resp.config.method !== 'get' && !resp.config.skipInterceptors) {
+  if (
+    resp.config.method !== 'get' &&
+    !resp.config.skipNotifications &&
+    !resp.config.skipSuccessNotification
+  ) {
     useSuccessNotification(resp)
   }
   return resp
