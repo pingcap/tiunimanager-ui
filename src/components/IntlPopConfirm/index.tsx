c4b289cd91879cc02a4cf0cfa6897a3a1a0e4c28
Yuiham <yuiham.chan@gmail.com>
2022-01-21 11:43:46 +0800

    refactor(components): increase IntlPopConfirm padding styles

--- src/components/IntlPopConfirm/index.tsx
+++ src/components/IntlPopConfirm/index.tsx
@@ -1,5 +1,6 @@
 import { Popconfirm, PopconfirmProps } from 'antd'
 import { loadI18n, useI18n } from '@i18n-macro'
+import styles from './index.module.less'
 
 loadI18n()
 
@@ -8,6 +9,7 @@ export default function IntlPopConfirm(props: PopconfirmProps) {
 
   return (
     <Popconfirm
+      overlayClassName={styles.intlPopConfirm}
       okText={t('ok')}
       cancelText={t('cancel')}
       okButtonProps={{ size: 'middle' }}
