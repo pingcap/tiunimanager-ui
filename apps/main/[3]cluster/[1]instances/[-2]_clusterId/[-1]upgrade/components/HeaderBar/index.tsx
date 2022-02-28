bee8aa63776e909ef0b71a6f1aa286e8f422431a
Yuiham <yuiham.chan@gmail.com>
2022-02-28 13:51:01 +0800

    feat(cluster/instances): support cluster upgrade

---
+++ apps/main/[3]cluster/[1]instances/[-2]_clusterId/[-1]upgrade/components/HeaderBar/index.tsx
@@ -0,0 +1,12 @@
+import { useI18n } from '@i18n-macro'
+import Header from '@/components/Header'
+
+export interface HeaderBarProps {
+  back: () => void
+}
+
+export default function HeaderBar({ back }: HeaderBarProps) {
+  const { t } = useI18n()
+
+  return <Header onBack={back} title={t('header.title')} />
+}
