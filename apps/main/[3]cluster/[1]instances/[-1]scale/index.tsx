3b7725f8b27420b02da76b1ddf0f73d0525e623c
Yuiham <yuiham.chan@gmail.com>
2022-03-30 20:39:34 +0800

    refactor(cluster): provide the topology detail list for clusters scaling out

--- apps/main/[3]cluster/[1]instances/[-1]scale/index.tsx
+++ apps/main/[3]cluster/[1]instances/[-1]scale/index.tsx
@@ -1,7 +1,11 @@
 import { useHistoryWithState } from '@/router/helper'
 import { resolveRoute } from '@pages-macro'
 import HeaderBar from './components/HeaderBar'
-import { ClusterInfo, ClusterRawTopologyItem } from '@/api/model'
+import {
+  ClusterComponentNodeInfo,
+  ClusterInfo,
+  ClusterRawTopologyItem,
+} from '@/api/model'
 import { Redirect } from 'react-router-dom'
 import { ScaleOutPanel } from '@apps/main/[3]cluster/[1]instances/[-1]scale/components/ScaleOutPanel'
 
@@ -9,9 +13,10 @@ export default function () {
   const history = useHistoryWithState<{
     cluster: ClusterInfo
     topology: ClusterRawTopologyItem[]
+    topologyDetails: ClusterComponentNodeInfo[]
     from: string
   }>()
-  const { cluster, from, topology } = history.location.state
+  const { cluster, from, topology, topologyDetails } = history.location.state
   if (!cluster) return <Redirect to={resolveRoute('../')} />
   const backPath = from || resolveRoute('../:clusterId', cluster.clusterId!)
   const backToPrevPage = () => history.push(backPath)
@@ -22,6 +27,7 @@ export default function () {
       <ScaleOutPanel
         cluster={cluster}
         topology={topology}
+        topologyDetails={topologyDetails}
         back={backToPrevPage}
       />
     </>
