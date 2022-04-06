62cc22dbc47c01d04d58036b35811ef26c86e4d3
Yuiham <yuiham.chan@gmail.com>
2022-04-06 14:55:06 +0800

    refactor(start): adjust the layout styles

--- apps/main/[-1]start/index.tsx
+++ apps/main/[-1]start/index.tsx
@@ -25,45 +25,43 @@ export default function () {
   }, [history])
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.layout}>
-        <div className={styles.main}>
-          <div className={styles.header}>
-            <div className={styles.headerTitle}>{t('header.title')}</div>
-            <div className={styles.headerDesc}>{t('header.desc')}</div>
-          </div>
-          <Steps className={styles.content} direction="vertical">
-            <Steps.Step
-              className={styles.step}
-              title={t('steps.dataCenter.title')}
-              description={t('steps.dataCenter.desc')}
-              status={
-                initInfo.vendorZone && initInfo.vendorSpec ? 'finish' : 'wait'
-              }
-            />
-            <Steps.Step
-              title={t('steps.productComponent.title')}
-              description={t('steps.productComponent.desc')}
-              status={initInfo.productComponent ? 'finish' : 'wait'}
-            />
-            <Steps.Step
-              title={t('steps.productVersion.title')}
-              description={t('steps.productVersion.desc')}
-              status={initInfo.productVersion ? 'finish' : 'wait'}
-            />
-          </Steps>
-          <Space>
-            <Button type="primary" onClick={handleBegin}>
-              {t('actions.begin')}
-            </Button>
-            <Button type="link" onClick={handleSkip}>
-              {t('actions.skip')}
-            </Button>
-          </Space>
+    <div className={styles.layout}>
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <div className={styles.headerTitle}>{t('header.title')}</div>
+          <div className={styles.headerDesc}>{t('header.desc')}</div>
         </div>
-        <div className={styles.aside}>
-          <img className={styles.bgImg} src={startImg} alt="Get Started" />
-        </div>
+        <Steps className={styles.content} direction="vertical">
+          <Steps.Step
+            className={styles.step}
+            title={t('steps.dataCenter.title')}
+            description={t('steps.dataCenter.desc')}
+            status={
+              initInfo.vendorZone && initInfo.vendorSpec ? 'finish' : 'wait'
+            }
+          />
+          <Steps.Step
+            title={t('steps.productComponent.title')}
+            description={t('steps.productComponent.desc')}
+            status={initInfo.productComponent ? 'finish' : 'wait'}
+          />
+          <Steps.Step
+            title={t('steps.productVersion.title')}
+            description={t('steps.productVersion.desc')}
+            status={initInfo.productVersion ? 'finish' : 'wait'}
+          />
+        </Steps>
+        <Space>
+          <Button type="primary" onClick={handleBegin}>
+            {t('actions.begin')}
+          </Button>
+          <Button type="link" onClick={handleSkip}>
+            {t('actions.skip')}
+          </Button>
+        </Space>
+      </div>
+      <div className={styles.aside}>
+        <img className={styles.bgImg} src={startImg} alt="Get Started" />
       </div>
     </div>
   )
