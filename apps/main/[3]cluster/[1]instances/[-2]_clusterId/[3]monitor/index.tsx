1d5cc55e0a8e1603f9e86e266a7e8db1b55b140d
Yuiham <Yuiham@users.noreply.github.com>
2022-06-14 15:19:39 +0800

    refactor(cluster/monitor): update the grafana dashboard link

--- apps/main/[3]cluster/[1]instances/[-2]_clusterId/[3]monitor/index.tsx
+++ apps/main/[3]cluster/[1]instances/[-2]_clusterId/[3]monitor/index.tsx
@@ -18,13 +18,28 @@ import { ExternalService } from '@/components/ExternalService'
 import { useQueryClusterExternalService } from '@/api/hooks/cluster'
 import { useClusterContext } from '@apps/main/[3]cluster/[1]instances/[-2]_clusterId/context'
 
+/**
+ * Get the Grafana dashboard URL of the cluster
+ * @param origin the Grafana origin
+ * @param clusterName the cluster name
+ */
+function getGrafanaUrl(origin?: string, clusterName?: string) {
+  if (!origin || !clusterName) {
+    return ''
+  }
+
+  const { hostname, port } = new URL(origin)
+
+  return `/grafanas-${hostname}-${port}/d/000000012/${clusterName}-tidb-summary?orgId=1&kiosk=tv`
+}
+
 export default function () {
   const { info } = useClusterContext()
   const { data } = useQueryClusterExternalService({ id: info!.clusterId! })
-  const grafanaUrl = data?.data.data?.grafanaUrl
-    ? `${data?.data.data?.grafanaUrl}/d/000000012/${
-        info!.clusterName
-      }-tidb-summary?orgId=1&kiosk=tv`
-    : ''
-  return <ExternalService src={grafanaUrl} mode="card" />
+  const grafanaUrl = getGrafanaUrl(
+    data?.data.data?.grafanaUrl,
+    info?.clusterName
+  )
+
+  return grafanaUrl ? <ExternalService src={grafanaUrl} mode="card" /> : null
 }
